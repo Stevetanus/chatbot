4700fb5fc45c03c48d60c3fc95c5364eddfba309
Thibetanus <steventhibe@gmail.com>
2024-02-24 21:23:27 +0800

    only /

--- api/index.py
+++ api/index.py
@@ -13,21 +13,21 @@ handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 def home():
     return 'Hello, World!'
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     signature = request.headers['X-Line-Signature']
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+#     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token, message)
 
 # if __name__ == "__main__":
 #     # port = int(os.environ.get('PORT', 5000))
