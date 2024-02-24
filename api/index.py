25710809b2764cc9120286c36549fc2caed85318
Thibetanus <steventhibe@gmail.com>
2024-02-24 21:39:11 +0800

    try to deploy on netlify

--- api/index.py
+++ api/index.py
@@ -1,49 +1,49 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, world'
-
-
-@app.route('/test')
-def test():
-    return 'Test'
-
-# from flask import Flask, request, abort
-# from linebot import LineBotApi, WebhookHandler
-# from linebot.exceptions import InvalidSignatureError
-# from linebot.models import *
-# import os
+# from flask import Flask, render_template
 
 # app = Flask(__name__)
 
-# line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
-# handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 # @app.route('/')
-# def home():
-#     return 'Hello, World!'
+# def hello():
+#     return 'Hello, world'
 
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     signature = request.headers['X-Line-Signature']
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-#     return 'OK'
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     message = TextSendMessage(text=event.message.text)
-#     line_bot_api.reply_message(event.reply_token, message)
+# @app.route('/test')
+# def test():
+#     return 'Test'
 
-# if __name__ == "__main__":
-#     # port = int(os.environ.get('PORT', 5000))
-#     # app.run(host='0.0.0.0', port=port)
-#     app.run()
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import os
+
+# app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+if __name__ == "__main__":
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
